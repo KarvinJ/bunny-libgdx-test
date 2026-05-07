820add3b8d4b7092f882b8112cdaf241e6a7179d
knight <karvin_jimenez@hotmail.com>
2026-05-06 20:57:08 -0400

    added font and some refactoring.

--- core/src/main/java/knight/nameless/Bunny.java
+++ core/src/main/java/knight/nameless/Bunny.java
@@ -2,47 +2,40 @@ package knight.nameless;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
 public class Bunny {
-    private Vector2 position;
-    private Vector2 velocity;
-    private Vector2 bounds;
+
+    private final Vector2 position;
+    private final Vector2 velocity;
     private final Texture texture;
+    private Vector2 screenBounds;
 
-    Bunny(Texture texture) {
+    Bunny(Texture texture, float positionX, float positionY) {
         this.texture = texture;
-        position = Vector2.Zero;
-        velocity = Vector2.Zero;
-        bounds = Vector2.Zero;
-    }
-
-
-    public void setPosition(Vector2 position) {
-        this.position = position;
-    }
-
-    public void setVelocity(Vector2 velocity) {
-        this.velocity = velocity;
-    }
-
-    public void setBounds(Vector2 bounds) {
-        this.bounds = bounds;
+        position = new Vector2(positionX, positionY);
+        velocity = new Vector2(MathUtils.random(-200, 200), MathUtils.random(-200, 200));
+        screenBounds = Vector2.Zero;
     }
 
     public void update(float deltaTime) {
 
-        position.mulAdd(velocity, deltaTime);
-
-        if (position.x <= 0 || position.x >= bounds.x)
+        if (position.x <= 0 || position.x >= screenBounds.x)
             velocity.x = -velocity.x;
 
-        if (position.y <= 0 || position.y >= bounds.y)
+        if (position.y <= 0 || position.y >= screenBounds.y)
             velocity.y = -velocity.y;
+
+        position.mulAdd(velocity, deltaTime);
     }
 
     public void draw(SpriteBatch batch) {
         batch.draw(texture, position.x, position.y);
+    }
+
+    public void setScreenBounds(Vector2 screenBounds) {
+        this.screenBounds = screenBounds;
     }
 
     public void dispose() {
--- core/src/main/java/knight/nameless/Main.java
+++ core/src/main/java/knight/nameless/Main.java
@@ -2,34 +2,43 @@ package knight.nameless;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Main extends ApplicationAdapter {
-    SpriteBatch batch;
-    BitmapFont font;
-    OrthographicCamera camera;
-    Array<Bunny> bunnies;
-    Texture bunnyTexture;
+
+    private final int SCREEN_WIDTH = 1024;
+    private final int SCREEN_HEIGHT = 768;
+    private SpriteBatch batch;
+    private BitmapFont font;
+    private OrthographicCamera camera;
+    private Array<Bunny> bunnies;
+    private Texture bunnyTexture;
+    private Texture fontTexture;
 
     @Override
     public void create() {
 
         batch = new SpriteBatch();
-        font = new BitmapFont();
+
+        fontTexture = new Texture("fonts/test.png");
+        fontTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        font = new BitmapFont(Gdx.files.internal("fonts/test.fnt"), new TextureRegion(fontTexture));
+        font.getData().scale(1f);
 
         bunnyTexture = new Texture("wabbit_alpha.png");
         bunnies = new Array<>();
 
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, 1024, 768);
+        camera.setToOrtho(false, SCREEN_WIDTH, SCREEN_HEIGHT);
     }
 
     private void addBunnies() {
@@ -40,11 +49,8 @@ public class Main extends ApplicationAdapter {
             touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
             camera.unproject(touchPos);
 
-            var bunny = new Bunny(bunnyTexture);
-
-            bunny.setPosition(new Vector2(touchPos.x, touchPos.y));
-            bunny.setVelocity(new Vector2(MathUtils.random(-200, 200), MathUtils.random(-200, 200)));
-            bunny.setBounds(new Vector2(1024, 768));
+            var bunny = new Bunny(bunnyTexture, touchPos.x, touchPos.y);
+            bunny.setScreenBounds(new Vector2(SCREEN_WIDTH, SCREEN_HEIGHT));
 
             bunnies.add(bunny);
         }
@@ -64,7 +70,7 @@ public class Main extends ApplicationAdapter {
     @Override
     public void render() {
 
-        ScreenUtils.clear(0, 1, 1, 1);
+        ScreenUtils.clear(Color.BLACK);
 
         update(Gdx.graphics.getDeltaTime());
 
@@ -74,8 +80,8 @@ public class Main extends ApplicationAdapter {
         for (var bunny : bunnies)
             bunny.draw(batch);
 
-        font.draw(batch, String.format("FPS: %d", Gdx.graphics.getFramesPerSecond()), 20, 20);
-        font.draw(batch, String.format("Count: %d", bunnies.size), 20, 50);
+        font.draw(batch, String.format("FPS: %d", Gdx.graphics.getFramesPerSecond()), 20, SCREEN_HEIGHT - 20);
+        font.draw(batch, String.format("Count: %d", bunnies.size), 20, SCREEN_HEIGHT - 80);
 
         batch.end();
     }
@@ -84,6 +90,8 @@ public class Main extends ApplicationAdapter {
     public void dispose() {
 
         batch.dispose();
+        font.dispose();
+        fontTexture.dispose();
         bunnies.forEach(Bunny::dispose);
     }
 }
